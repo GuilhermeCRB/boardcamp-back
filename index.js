6916238a75092e768e37541a387185ff1e2aac78
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-05-25 16:09:51 -0300

    feat: starts project

---
+++ index.js
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import cors from "cors";
+import chalk from "chalk";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(json());
+app.use(cors());
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(chalk.white.bold.bgGreenBright(`\n Application is running on port ${port}... \n`)));
