e10897e7fc1496c0f8b1c06df7ee9d1594546f8d
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-05-27 15:56:27 -0300

    feat: creates routes

--- index.js
+++ index.js
@@ -14,6 +14,5 @@ app.use(gamesRouter);
 app.use(costumersRouter);
 app.use(rentalsRouter);
 
-
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(chalk.white.bold.bgGreenBright(`\n Application is running on port ${port}... \n`)));
---
+++ routes/costumersRouter.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const costumersRouter = Router();
+
+export default costumersRouter;
