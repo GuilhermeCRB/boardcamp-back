ae03fd2bbc7b0aa2a2bd097fe4fdf161011eea60
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-05-28 10:16:29 -0300

    feat: validates category input on post request

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -2,13 +2,24 @@ import chalk from "chalk";
 
 import db from "../database-boardcamp/db.js";
 
-export async function getCategories(req, res){
-    try{
+export async function getCategories(req, res) {
+    try {
         const categoriesQuery = await db.query("SELECT * FROM categories");
         const categoriesList = categoriesQuery.rows;
         res.status(200).send(categoriesList);
-    }catch(e){
+    } catch (e) {
         res.status(500).send(e);
         console.log(chalk.red.bold("\nAn error occured while trying do get categories."));
+    }
+}
+
+export async function createCategory(req, res) {
+    const { category } = res.locals;
+    try {
+        await db.query("INSERT INTO categories(name) VALUES ($1)", [category.name]);
+        res.sendStatus(201);
+    } catch (e) {
+        res.status(500).send(e);
+        console.log(chalk.red.bold("\nAn error occured while trying to create a category."));
     }
 }
---
+++ middlewares/categoriesMiddleware.js
@@ -0,0 +1,36 @@
+import { stripHtml } from "string-strip-html";
+import Joi from "joi";
+import db from "../database-boardcamp/db.js";
+
+export async function validateCategory(req, res, next) {
+    const categoryReceived = req.body;
+
+    const category = { ...categoryReceived, name: stripHtml(categoryReceived.name).result };
+
+    const categorySchema = Joi.object({
+        name: Joi.string().required()
+    });
+
+    const validation = categorySchema.validate(category);
+
+    if (validation.error && validation.error._original.name === "") {
+        return res.status(400).send(validation.error.details[0].message);
+    }
+
+    try {
+        const categoriesQuery = await db.query("SELECT * FROM categories");
+        const categoriesList = categoriesQuery.rows;
+
+        const thereIsCategory = categoriesList.filter(categoryFromDB => categoryFromDB.name.toLowerCase() === category.name.toLowerCase());
+        if (thereIsCategory) {
+            return res.status(409).send("Category already exists!");
+        }
+
+        res.locals.category = category;
+
+        next();
+    } catch (e) {
+        res.status(500).send(e);
+        console.log(chalk.red.bold("\nAn error occured while trying to verify if category already exists."));
+    }
+}
