68df34604a74fef475f4307c507c658d3f7350a1
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-05-29 19:07:31 -0300

    feat: updates customer data on customers/id put request

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -14,3 +14,24 @@ export async function createCustomer(req, res) {
         return res.status(500).send(e);
     }
 }
+
+export async function updateCustomer(req, res) {
+    const { id } = req.params;
+    const { customer } = res.locals;
+    const values = Object.values(customer);
+
+    try {
+        await db.query(`
+            UPDATE customers SET
+                name = $1,
+                phone = $2,
+                cpf = $3,
+                birthday = $4
+            WHERE id = $5;
+        `, [...values, id])
+        return res.sendStatus(200);
+    } catch (e) {
+        console.log(chalk.red.bold("\nAn error occured while trying to update customer."));
+        return res.status(500).send(e);
+    }
+}
