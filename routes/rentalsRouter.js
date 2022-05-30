3562d490c10ba5fd94d46084a9d1bde6cbba1f6a
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-05-29 23:02:13 -0300

    feat: creates rental on rentals post request

--- routes/rentalsRouter.js
+++ routes/rentalsRouter.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import { createRental } from "../controllers/rentalsController.js";
+
 const rentalsRouter = Router();
+
+rentalsRouter.post("/rentals", createRental);
 
 export default rentalsRouter;
